13dabb0af491e27f35206051b7b64f79cc3e7002
dimasrangga <derangga1011@gmail.com>
2025-03-02 10:05:36 +0700

    layouting home

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
 "use client";
 
-import { Button, Input } from "@heroui/react";
-import { Search } from "lucide-react";
+import { RecipeCardLG } from "@/components/recipe-card-lg";
+import {
+  Button,
+  Input,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@heroui/react";
+import { CircleUserRound, Earth, Heart, Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const ref = useRef(null);
+  const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     if (!ref.current) return;
@@ -27,11 +38,19 @@ export default function Home() {
     };
   }, []);
 
+  const onCornerMenuAction = (key) => {
+    if (key === "sign-out") {
+      setIsLogin(false);
+    } else {
+      router.push(`/${key}`);
+    }
+  };
+
   return (
     <>
       <header
         ref={ref}
-        className="flex flex-row sticky top-0 w-screen px-8 h-16 items-center justify-center z-50 data-[scrolled]:shadow-md data-[scrolled]:backdrop-blur-xl transition-all"
+        className="flex flex-row sticky top-0 w-screen bg-white px-8 h-16 items-center justify-center z-50 data-[scrolled]:shadow-md data-[scrolled]:bg-white transition-all"
       >
         <div className="grow">
           <div className="flex flex-row items-center space-x-4">
@@ -64,111 +83,160 @@ export default function Home() {
             }
             classNames={"w-44"}
           />
-          <Link href={"/auth"}>
-            <Button variant="light">Log in</Button>
-          </Link>
-          <Link href={"/auth?tab=sign-up"}>
-            <Button className="border-black font-mono" variant="bordered">
-              Sign up
-            </Button>
-          </Link>
+          {isLogin ? (
+            <Dropdown>
+              <DropdownTrigger>
+                <div className="flex justify-center items-center w-10 h-10 bg-black rounded-full hover:cursor-pointer">
+                  <div className="text-white font-mono font-semibold">D</div>
+                </div>
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label="Static Actions"
+                onAction={(key) => onCornerMenuAction(key)}
+              >
+                <DropdownItem key="profile">Profile</DropdownItem>
+                <DropdownItem key="setting">Settings</DropdownItem>
+                <DropdownItem
+                  key="sign-out"
+                  className="text-danger"
+                  color="danger"
+                >
+                  Sign out
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          ) : (
+            <div className="space-x-2">
+              <Link href={"/auth"}>
+                <Button variant="light">Log in</Button>
+              </Link>
+              <Link href={"/auth?tab=sign-up"}>
+                <Button className="border-black font-mono" variant="bordered">
+                  Sign up
+                </Button>
+              </Link>
+            </div>
+          )}
         </div>
       </header>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            <li className="mb-2">
-              Get started by editing{" "}
-              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                src/app/page.js
-              </code>
-              .
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read our docs
-            </a>
+      <main className="flex flex-col w-screen py-6 px-8">
+        <section>
+          <div className="relative w-full rounded-xl h-[30rem] overflow-hidden">
+            <Image
+              src={"/assets/backdrop-food.png"}
+              fill
+              style={{ objectFit: "cover" }}
+              alt="header-image"
+              className="object-[100%_75%]"
+            />
+            <div className="absolute h-full xl:w-1/2 lg:w-2/3">
+              <div className="max-w-[38rem] mx-auto flex flex-col items-center mt-16 space-y-6">
+                <div className="font-poppins text-7xl font-bold text-white">
+                  Adventure of
+                  <span className="text-orange-400">delicious</span>
+                </div>
+                <div className="font-poppins text-slate-200 mx-auto text-lg">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat
+                </div>
+              </div>
+            </div>
           </div>
-        </main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
-      </div>
+        </section>
+        <section>
+          <div className="flex flex-row w-full h-52 mt-6 space-x-5">
+            <div className="grow rounded-xl bg-slate-100">
+              <div className="grid grid-cols-3 gap-4 h-full">
+                <div className="xl:w-72 lg:w-44 flex flex-col p-3">
+                  <div className="p-2 w-fit rounded-full bg-white">
+                    <CircleUserRound />
+                  </div>
+                  <div className="font-bold mt-4">Fun Comunity</div>
+                  <p className="mt-1 line-clamp-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+                <div className="xl:w-72 lg:w-44 flex flex-col p-3">
+                  <div className="p-2 w-fit rounded-full bg-white">
+                    <Earth />
+                  </div>
+                  <div className="font-bold mt-4">Fun Comunity</div>
+                  <p className="mt-1 line-clamp-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+                <div className="xl:w-72 lg:w-44 flex flex-col p-3">
+                  <div className="p-2 w-fit rounded-full bg-white">
+                    <Heart />
+                  </div>
+                  <div className="font-bold mt-4">Fun Comunity</div>
+                  <p className="mt-1 line-clamp-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <Link
+              href={"/ai-chef"}
+              className="w-48 flex flex-col rounded-xl border border-slate-100 bg-slate-50 shadow-lg"
+            >
+              <div className="grow relative w-full rounded-t-xl overflow-hidden">
+                <Image
+                  src={"/assets/ai-chef.webp"}
+                  fill
+                  style={{ objectFit: "cover" }}
+                  alt="header-image"
+                />
+              </div>
+              <div className="flex flex-col justify-center h-10 px-3">
+                <div className="font-poppins font-semibold text-orange-400 text-lg hover:underline">
+                  Talk with AI-Chef
+                </div>
+              </div>
+            </Link>
+            <div className="w-48 flex flex-col rounded-xl border border-slate-100 bg-slate-50 shadow-lg p-3">
+              <div className="font-poppins font-semibold text-orange-400 text-lg ">
+                FEATURED
+              </div>
+              <div className="flex flex-col grow justify-between mt-1">
+                <div className="font-poppins font-bold text-xl">
+                  Ranch Chicken and Rice
+                </div>
+                <div className=""></div>
+                <Link
+                  className="font-poppins text-sm underline font-semibold"
+                  href={"/recipe/id"}
+                >
+                  See recipe
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="flex flex-col py-8">
+            <div className="text-4xl font-poppins font-semibold">
+              Popular <span className="text-orange-400">Recipes</span>
+            </div>
+            <div className="grid xl:grid-cols-5 lg:grid-cols-4 gap-x-4 mt-6">
+              <RecipeCardLG />
+              <RecipeCardLG />
+              <RecipeCardLG />
+              <RecipeCardLG />
+              <RecipeCardLG />
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer></footer>
     </>
   );
 }
