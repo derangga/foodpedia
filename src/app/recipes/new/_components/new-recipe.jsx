99ce5e991312c1b59ab7abe4120ae5bc76ee7c3d
Dimas Rangga <derangga1011@gmail.com>
2025-03-05 05:42:05 +0700

    chore: decouple avatar menu (#6)

--- src/app/recipes/new/_components/new-recipe.jsx
+++ src/app/recipes/new/_components/new-recipe.jsx
@@ -1,20 +1,11 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import TipTap from "../../_components/tiptap";
-import {
-  Button,
-  Chip,
-  Dropdown,
-  DropdownTrigger,
-  DropdownItem,
-  DropdownMenu,
-  Input,
-  Select,
-  SelectItem,
-} from "@heroui/react";
-import { Trash2, SquareUserRound, Settings, LogOut } from "lucide-react";
+import { Button, Chip, Input, Select, SelectItem } from "@heroui/react";
+import { Trash2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { AvatarMenu } from "@/shared/components/avatar-menu";
 
 export const NewRecipe = ({ currentUser }) => {
   const ref = useRef(null);
@@ -45,10 +36,6 @@ export const NewRecipe = ({ currentUser }) => {
   }, []);
 
   const onCornerMenuAction = (key) => {};
-
-  const avatar = () => {
-    return currentUser.email.charAt(0).toUpperCase();
-  };
 
   const onAddIngridients = (e) => {
     e.preventDefault();
@@ -97,42 +84,10 @@ export const NewRecipe = ({ currentUser }) => {
             >
               Publish
             </button>
-            <Dropdown>
-              <DropdownTrigger>
-                <div className="flex justify-center items-center w-10 h-10 bg-black rounded-full hover:cursor-pointer">
-                  <div className="text-white font-mono font-semibold">
-                    {avatar()}
-                  </div>
-                </div>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Static Actions"
-                onAction={(key) => onCornerMenuAction(key)}
-              >
-                <DropdownItem key="profile">
-                  <div className="flex flex-row space-x-4 items-center">
-                    <SquareUserRound size={20} />
-                    <div>Profile</div>
-                  </div>
-                </DropdownItem>
-                <DropdownItem key="setting">
-                  <div className="flex flex-row space-x-4 items-center">
-                    <Settings size={20} />
-                    <div>Settings</div>
-                  </div>
-                </DropdownItem>
-                <DropdownItem
-                  key="sign-out"
-                  className="text-danger"
-                  color="danger"
-                >
-                  <div className="flex flex-row space-x-4 items-center">
-                    <LogOut size={20} />
-                    <div>Sign out</div>
-                  </div>
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
+            <AvatarMenu
+              name={currentUser.email}
+              onCornerMenuAction={onCornerMenuAction}
+            />
           </div>
         </div>
       </header>
