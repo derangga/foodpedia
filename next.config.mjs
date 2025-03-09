4918f7ac678b6812daf366345a55cc688073ec1f
dimasrangga <derangga1011@gmail.com>
2025-03-09 13:15:26 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,6 @@ const nextConfig = {
         hostname: "pub-e574a04c58f3409cb2cc91bd38277137.r2.dev",
         pathname: "/foodpedia/**",
       },
-      {
-        protocol: "https",
-        hostname: "www.foodandwine.com",
-      },
     ],
   },
 };
