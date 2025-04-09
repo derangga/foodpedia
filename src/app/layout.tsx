05ace26b790edb6f3faabdffde163e482263820a
Dimas Rangga <derangga1011@gmail.com>
2025-04-09 09:50:02 +0700

    feat: add metadata (#35)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./_components/providers";
@@ -12,9 +13,25 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Foodpedia",
   description: "Food recipe explorer",
+  openGraph: {
+    type: "website",
+    title: "Foodpedia",
+    description:
+      "A web app food recipe that user can share recipe and ask AI to give food recipe suggestion",
+    url: "https://foodpedia-orcin.vercel.app/",
+    siteName: "Foodpedia",
+    images: [
+      {
+        url: "https://pbs.twimg.com/media/GoD5RDXbwAMLVnD?format=jpg&name=4096x4096",
+        width: 1200,
+        height: 630,
+        alt: "Foodpedia",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
